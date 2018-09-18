e18b5b9c47eed708f8b4f1a06d2cf638b083ba6e
ytc1147819590 <34883875+ytc1147819590@users.noreply.github.com>
2018-09-18 13:06:01 +0800

    a

---
+++ project/server/db.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2018/9/18.
+ */
+
+//链接数据库
+
+module.exports = {
+    mysql : {
+      host:"localhost",//本地
+      user:"root",//用户名
+      password:"admin",//数据库密码
+      database:"test",//数据库名
+      port:"3306"//端口
+    }
+}
