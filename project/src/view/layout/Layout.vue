2891b30a222eceafd994fb31f2f9c61fa4426aab
ying <1147819590@qq.com>
2018-12-10 09:06:45 +0800

    a

---
+++ project/src/view/layout/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <vue-header></vue-header>
+    <slide-nav></slide-nav>
+  </div>
+</template>
+
+<script>
+  import vueHeader from './header'
+  import slideNav from './slideNav'
+  export default {
+    components: {
+      vueHeader,
+      slideNav
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
