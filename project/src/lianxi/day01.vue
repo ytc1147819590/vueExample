f56ab6ecc8eccbb9b5fe36e57d19f95c27d80ebf
ying <1147819590@qq.com>
2018-12-17 08:51:50 +0800

    1

--- project/src/lianxi/day01.vue
+++ project/src/lianxi/day01.vue
@@ -1,11 +1,40 @@
 <template>
   <div>
-    1
+    <Table v-bind="table"></Table>
   </div>
 </template>
 
 <script>
-  export default {}
+  import Table from './day02'
+  export default {
+    components: {
+      Table
+    },
+    data() {
+      return {
+        table: {
+          list: [{name: '11', phone:'22'}, {name: '111', phone: '222'}],
+          tableHeader: [
+            {label: '名字', name: 'name'},
+            {label: '手机号', name: 'phone'},
+            {label: '用户名', name: 'user'},
+            {label: '密码', name: 'password'},
+            {label: '地址', name: 'address'},
+          ],
+          operation: {
+            show: true,
+            label: '操作',
+            list: [
+              { label: '查看', type: 'primary' },
+              { label: '添加'},
+              { label: '删除', type: 'danger' }
+            ],
+            radio: 'tt'
+          }
+        }
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
--- project/src/lianxi/day02.vue
+++ project/src/lianxi/day02.vue
@@ -1,11 +1,56 @@
 <template>
   <div>
-   2
+    <el-table :data="list" border>
+      <el-table-column v-if="multiple" type="selection" width="55"></el-table-column>
+      <el-table-column v-if="multiple" label="选择">
+        <template slot-scope="scope">
+          <el-radio v-model="radio"></el-radio>
+        </template>
+      </el-table-column>
+      <el-table-column v-for="(item, index) in tableHeader" :prop="item.name" :label="item.label" :key="index"></el-table-column>
+      <el-table-column :label="operation.label" v-if="operation.show">
+        <template slot-scope="scope">
+          <el-button v-for="(item, index) in operation.list" :type="item.type">{{item.label}}</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <div>{{radio}}</div>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data() {
+      return {
+      }
+    },
+    props: {
+      multiple: {
+        type: Boolean,
+        default: false
+      },
+      list: {
+        type: Array,
+        default() {
+          return []
+        }
+      },
+      tableHeader: {
+        type: Array,
+        default() {
+          return []
+        }
+      },
+      operation: {
+        type: Object,
+        default() {
+          return {}
+        }
+      },
+      radio: {}
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
