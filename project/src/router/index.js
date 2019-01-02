41c96101f425c359b898ead3d12e047bcf11d34f
ytc1147819590 <34883875+ytc1147819590@users.noreply.github.com>
2019-01-02 10:22:14 +0800

    拦截器

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -2,27 +2,51 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Layout from '@/view/layout/Layout'
+import store from '@/store/index'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/day01',
-      name: '/day01',
-      component: () => import('@/lianxi/day01')
-    }, {
-      path: '/day02',
-      name: '/day02',
-      component: () => import('@/lianxi/day02')
-    }, {
-      path: '/day03',
-      name: '/day03',
-      component: () => import('@/lianxi/day03')
-    }, {
-      path: '/user',
-      name: '/user',
-      component: () => import('@/lianxi/user')
-    }
-  ]
+export const routes = [
+  {
+    path: '/day01',
+    name: '/day01',
+    component: () => import('@/lianxi/day01')
+  }, {
+    path: '/day02',
+    name: '/day02',
+    component: () => import('@/lianxi/day02')
+  }, {
+    path: '/day03',
+    name: '/day03',
+    meta: {
+      requireAuth: true
+    },
+    component: () => import('@/lianxi/day03')
+  }, {
+    path: '/user',
+    name: '/user',
+    component: () => import('@/lianxi/user')
+  }
+]
+
+const router = new Router({
+  routes
 })
+
+export default router
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) {
+    if (store.state.user) {
+      next()
+    } else {
+      next({
+        path: '/',
+        query: { id: '123' }
+      })
+    }
+  } else {
+    next()
+  }
+})
+
